e44b4909e899385ffd718b32cdec5aa8494533e4
전성호 <kopasd99@gmail.com>
2022-09-03 17:39:24 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,76 @@
-import { createGlobalStyle } from "styled-components";
-import ToDoList from "./components/ToDoList";
+import styled from "styled-components";
+import { DragDropContext ,Droppable,Draggable,DropResult} from "react-beautiful-dnd";
+import { useRecoilState } from 'recoil';
+import { toDoState } from "./atoms";
+import DraggableCard from "./Components/DragabbleCard";
 
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup, 
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  font-family: 'Source Sans Pro', sans-serif;
-  background-color:${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor}
-}
-a {
-  text-decoration:none;
-  color:inherit;
-}
-`;
+import Board from './Components/Board';
+ const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+ `;
+
+ const Boards = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+  gap: 10px;
+ `;
+
+ const Card = styled.div`
+   border-radius: 5px;
+   margin-bottom: 5px;
+   padding: 10px 10px;
+   background-color: ${(props) => props.theme.cardColor};
+ `; 
 
 function App() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = (info: DropResult) => {
+    console.log(info);
+    const { destination, draggableId, source } = info;
+    if (!destination) return;
+    if (destination?.droppableId === source.droppableId) {
+      // same board movement.
+      setToDos((allBoards) => {
+      const boardCopy = [...allBoards[source.droppableId]];
+      boardCopy.splice(source.index, 1);
+      boardCopy.splice(destination?.index, 0, draggableId);
+      return {
+        ...allBoards,
+        [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination.droppableId !== source.droppableId) {
+      // cross board movement
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
+        };
+      });
+    }
+  };
   return (
     <>
-      <GlobalStyle />
-      <ToDoList />
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Wrapper>
+          <Boards>  
+            {Object.keys(toDos).map(boardId => <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />)}
+          </Boards>
+        </Wrapper>
+      </DragDropContext>
     </>
   );
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,67 +1,14 @@
 import { atom, selector } from "recoil";
 
- export enum Categories {
-   "TO_DO" = "TO_DO",
-   "DOING" = "DOING",
-   "DONE" = "DONE",
- }
- export interface IToDo {
-   text: string;
-   id: number;
-   category: Categories;
- }
+interface IToDoState{
+  [key: string]: string[];
+}
 
- export const categoryState = atom<Categories>({
-   key: "category",
-   default: Categories.TO_DO,
- });
-
- export const toDoState = atom<IToDo[]>({
+export const toDoState = atom<IToDoState>({
   key: "toDo",
-  default: [],
-});
-export const toDoSelector = selector({
-  key: "toDoSelector",
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+  default: {
+    "To do": ["a", "b"],
+    Doing: ["c", "d", "e"],
+    Done: ["f"],
   },
 });
-
-/* 
-export const minuteState = atom({
-  key="mimutes",
-  default:0,
-});
-export const hourSelector = selector({
-  key:"hours",
-  /* get함수는 atom의 값을 가져온다 
-    get:({ get }) =>{
-      const minutes = get(minuteState);
-      return minutes/60;
-    },
-  });
-*/
-
-/* 
-  function App(){
-    const [minutes,setMinutes]=useRecoilState(minuteState);
-    const hours = useRecoilValue(hourSelector);
-    const onMinutesChange = (event:React.FormEvent<HTMLInputElement>)(
-      setMinutes(+event.currentTarget.value);
-    )
-
-    return(
-      <div>
-        <input
-          value={minutes}
-          onChange={onMinutesChange}
-          type="number" 
-          placeholder="Minutes"
-        />
-        <input value={hours} type="number" placeholder="Hours"  />
-      </div>
-    );
-  }
-*/
